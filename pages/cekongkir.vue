137aaf749b22d95599bb6f44fab2aea9ae7f65ce
dewaqintoro <dewaqintoro@gmail.com>
2021-04-05 22:13:03 +0700

    design kurir

--- pages/cekongkir.vue
+++ pages/cekongkir.vue
@@ -12,78 +12,94 @@
         </div>
       </header>
     </div>
-    <div class="w-2/3 mx-auto bg-white p-16 my-8 rounded-3xl">
-      <div class="flex">
-        <form class="w-1/2 border-r-2">
-          <div class="flex flex-wrap mb-6 w-full">
-            <div class="w-full px-3 mb-6 md:mb-0">
-              <h1 class="font-bold text-xl">Alamat Asal</h1>
-              <label class="block  tracking-wide text-black text-xs font-semibold mt-4 mb-3" for="grid-first-name">
-                Pilih Provinsi
-              </label>
-              <div class="relative">
-                <select v-model="asalProv" @change="setAsalCity()" class="block appearance-none w-full border border-gray-200 text-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                  <option v-for="(prov, index) in province" :key="index" :value="prov.province_id">{{prov.province}}</option>
-                </select>
+    <div class="flex my-8 px-8 justify-between">
+      <div class="w-4/6 p-8">
+        <div class="bg-white p-8 rounded-3xl">
+          <div class="flex">
+            <form class="w-1/2 border-r-2">
+              <div class="flex flex-wrap mb-6 w-full">
+                <div class="w-full px-3 mb-6 md:mb-0">
+                  <h1 class="font-bold text-xl">Alamat Asal</h1>
+                  <label class="block  tracking-wide text-black text-xs font-semibold mt-4 mb-3" for="grid-first-name">
+                    Pilih Provinsi
+                  </label>
+                  <div class="relative">
+                    <select v-model="asalProv" @change="setAsalCity()" class="block appearance-none w-full border border-gray-200 text-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                      <option v-for="(prov, index) in province" :key="index" :value="prov.province_id">{{prov.province}}</option>
+                    </select>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="flex flex-wrap mb-6 w-full">
-            <div class="w-full px-3 mb-6 md:mb-0">
-              <label class="block  tracking-wide text-black text-xs font-semibold mt-4 mb-3" for="grid-first-name">
-                Pilih Kota
-              </label>
-              <div class="relative">
-                <select v-model="asalCity" class="block appearance-none w-full border border-gray-200 text-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                  <option v-for="(x, index) in cityAsal" :key="index" :value="x.city_id">{{x.city_name}}</option>
-                </select>
+              <div class="flex flex-wrap mb-6 w-full">
+                <div class="w-full px-3 mb-6 md:mb-0">
+                  <label class="block  tracking-wide text-black text-xs font-semibold mt-4 mb-3" for="grid-first-name">
+                    Pilih Kota
+                  </label>
+                  <div class="relative">
+                    <select v-model="asalCity" class="block appearance-none w-full border border-gray-200 text-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                      <option v-for="(x, index) in cityAsal" :key="index" :value="x.city_id">{{x.city_name}}</option>
+                    </select>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="flex flex-wrap mb-2">
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-              <label class="block tracking-wide text-black text-xs font-bold mb-2" for="grid-city">
-                Berat (gram)
-              </label>
-              <input v-model="berat" @change="cek($event)"  class="appearance-none block w-full text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="1000">
-            </div>
-          </div>
-        </form>
-        <form class="w-1/2 border-l-2">
-          <div class="flex flex-wrap mb-6 w-full">
-            <div class="w-full px-3 mb-6 md:mb-0">
-              <h1 class="font-bold text-xl">Alamat Tujuan</h1>
-              <label class="block tracking-wide text-black text-xs font-semibold mt-4 mb-3" for="grid-first-name">
-                Pilih Provinsi
-              </label>
-              <div class="relative">
-                <select v-model="selectedProv" @change="setCity()" class="block appearance-none w-full border border-gray-200 text-black py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                  <option v-for="(prov, index) in province" :key="index" :value="prov.province_id">{{prov.province}}</option>
-                </select>
+              <div class="flex flex-wrap mb-2">
+                <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                  <label class="block tracking-wide text-black text-xs font-bold mb-2" for="grid-city">
+                    Berat (gram)
+                  </label>
+                  <input v-model="berat" @change="cek($event)"  class="appearance-none block w-full text-black border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="text" placeholder="1000">
+                </div>
               </div>
-            </div>
-          </div>
-          <div class="flex flex-wrap mb-6 w-full">
-            <div class="w-full px-3 mb-6 md:mb-0">
-              <label class="block  tracking-wide text-black text-xs font-semibold mt-4 mb-3" for="grid-first-name">
-                Pilih Kota
-              </label>
-              <div class="relative">
-                <select v-model="selectedCity" class="block appearance-none w-full border border-gray-200 text-black py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                  <option v-for="(x, index) in city" :key="index" :value="x.city_id">{{x.city_name}}</option>
-                </select>
+            </form>
+            <form class="w-1/2 border-l-2">
+              <div class="flex flex-wrap mb-6 w-full">
+                <div class="w-full px-3 mb-6 md:mb-0">
+                  <h1 class="font-bold text-xl">Alamat Tujuan</h1>
+                  <label class="block tracking-wide text-black text-xs font-semibold mt-4 mb-3" for="grid-first-name">
+                    Pilih Provinsi
+                  </label>
+                  <div class="relative">
+                    <select v-model="selectedProv" @change="setCity()" class="block appearance-none w-full border border-gray-200 text-black py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                      <option v-for="(prov, index) in province" :key="index" :value="prov.province_id">{{prov.province}}</option>
+                    </select>
+                  </div>
+                </div>
               </div>
-            </div>
+              <div class="flex flex-wrap mb-6 w-full">
+                <div class="w-full px-3 mb-6 md:mb-0">
+                  <label class="block  tracking-wide text-black text-xs font-semibold mt-4 mb-3" for="grid-first-name">
+                    Pilih Kota
+                  </label>
+                  <div class="relative">
+                    <select v-model="selectedCity" class="block appearance-none w-full border border-gray-200 text-black py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+                      <option v-for="(x, index) in city" :key="index" :value="x.city_id">{{x.city_name}}</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
+          <button @click="cekBiaya()" class="focus:outline-none bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            Cek biaya
+          </button>
+        </div>
       </div>
-      <button @click="cekBiaya()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-        Cek biaya
-      </button>
-      <div>
-        
+      <div class="w-2/6 p-8">
+        <div class="bg-white p-8 rounded-3xl">
+          <div class="text-center">
+            <p class="text-lg font-bold">Kurir</p>
+          </div>
+          <div class="mt-4">
+            <div v-for="(x, index) in biaya" :key="index" class="kurir flex mb-4 p-2 rounded-xl">
+              <!-- <input type="radio" :id="x.service" :value="x.cost[0].value" class="my-auto" v-model="picked"> -->
+              <label for="one" class="ml-4">
+                <h1>JNE</h1>
+                <p>{{x.service}} : {{x.cost[0].value}}</p>
+              </label>
+            </div>
+          </div>
+        </div>
       </div>
-      <p v-for="(x, index) in biaya" :key="index">{{x.cost[0].value}}</p>
     </div>
   </div>
 </template>
@@ -103,6 +119,7 @@ export default {
     const selectedCity = ref('')
     const asalCity = ref('')
     const biaya = ref('')
+    const picked = ref('')
 
     setProv()
 
@@ -116,6 +133,7 @@ export default {
       selectedProv,
       selectedCity,
       biaya,
+      picked,
       cek,
       setProv,
       setCity,
@@ -163,5 +181,8 @@ export default {
   width: 5px;
   height: 30vh;
   background: black;
+}
+.kurir {
+  background: #0fbcf9;
 }
 </style>
