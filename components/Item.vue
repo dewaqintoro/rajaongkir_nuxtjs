ff25ac1f826c880afe13add68d54f38fdd1158f7
dewaqintoro <dewa.qintoro@students.amikom.ac.id>
2021-04-18 12:52:42 +0700

    surat by id v1

---
+++ components/Item.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="footer mt-16 py-12 bg-primary text-white">
+    <div class="w-2/3 flex center item-center mx-auto">
+      <div class="w-2/3 flex">
+        <div class="pl-4 pr-16">
+          <p class="font-semibold text-xl">Kementerian Koordinator Bidang Perekonomian Republik Indonesia</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from '@nuxtjs/composition-api'
+export default {
+  name: 'Item',
+  setup(){
+
+    return { }
+  }
+}
+</script>
+
+<style scoped>
+.footer img {
+  height: 40px;
+  width: 40px;
+}
+.footer ul li{
+  margin-bottom: 1rem;
+}
+</style>
--- pages/surah/_id.vue
+++ pages/surah/_id.vue
@@ -1,11 +1,27 @@
 <template>
   <span>
-    <div v-if="surah">
-      <nuxt-link to="/quran">
+    <button @click="mergeArrayObjects()">cek</button>
+    <nuxt-link to="/quran">
         Home
       </nuxt-link>
-      <br/>
-      <p class="text-2xl font-arabic" v-for="(x, index) in surah.text" :key="index">{{x}}</p>
+    <hr/>
+    <!-- <div class="item"  v-for="(surat, index) in surah.text" :key="surat.index">
+      <p>{{surat}}</p>
+      <p>{{surah.translations.id.text[index]}}</p>
+    </div> -->
+    <div v-if="surah">
+      
+      <div class="item"  v-for="(x, index) in surah.text" :key="x.index">
+        <div class="card">
+          <div class="flex">
+            <div class="nameSurah">
+              <p>{{x}}</p>
+              <p>{{surah.translations.id.text[index]}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+        
     </div>
   </span>
 </template>
@@ -21,36 +37,138 @@ export default {
     const selectedCity = ref('')
     const asalCity = ref('')
     const surah = useAsync(async () => await getSurah())
-    const allSurah = useAsync(async () => await getAllSurah())
+    // const surah = ref('anjing')
+
+    const arr1 =[
+      {id:1,name:"sai"},
+      {id:2,name: "King"}
+    ];
+    const arr2 = [
+        {id:1,age:23},
+        {id:2,age:24}
+    ];
+
+    const iniSurat = surah.value?.text
+    const arti = surah.value?.translations?.id?.text
 
     return {
       surah,
-      allSurah,
-      cek
+      cek,
+      mergeArrayObjects
     }
+
+    function mergeArrayObjects1(arr1,arr2){
+      return arr1.map((item,i)=>{
+        if(item.id === arr2[i].id){
+          return Object.assign({},item,arr2[i])
+        }
+      })
+    }
+
+    function mergeArrayObjects(){
+      // Object.entries(surah.value?.text).forEach(function(query, i){
+      //   console.log(query[0])
+      //   console.log(i+1)
+      //   console.log(surah.value?.translations?.id)
+      // })
+      var array1 = [[1,2,3,4],[4,5,6,7]];
+      var array2 = [['a','b','c','d'],['e','f','g','h']];
+
+      var array3 = array1.map(function (e, i) {
+          return [e, array2[i]];
+      });
+
+      console.log(array3);
+    }
+    
 
     async function getSurah(){
       const resp = await import(`~/data/surah/${idParams}.json`)
-      // console.log(resp[idParams])
       return resp[idParams]
     }
 
-    async function getAllSurah(){
-      const resp = await import('~/data/surah-info.json')
-      // console.log(resp)
-      return resp.surah_info
+    async function cek1(){
+      // console.log(allSurah?.value)
+      // surah.map((x) => {
+      //   console.log(x)
+      // })
+
+      const surat = surah.value.text
+      console.log(surat)
+
+      // surat.forEach((a) => {
+      //   console.log(a)
+      // })
+      // var obj = {a:1,b:2};
+
+      Object.entries(surat).forEach(function(query){
+          console.log(query)
+      })
     }
+    
 
     async function cek(){
-      // console.log(allSurah?.value)
-      console.log('idParams', idParams)
+      // console.log(mergeArrayObjects(surat,arti));
+      // let employee = {
+      //     ...surat,
+      //     ...arti
+      // }
+      // console.log(employee)
+      // var a = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+      // var b = [ "foo", "bar", "baz", "bam", "bun", "fun" ];
+
+      // var array1 = [
+      //   {name: "lang", value: "English"}, 
+      //   {name: "age", value: "18"}
+      // ];
+      // var array2 = [
+      //   {code: "EN", text: "English language"}, 
+      //   {code: "DE", value: "German", text: "German language"}
+      // ];
+
+      // var array3 = array1.map((obj, index) => Object.assign({}, obj, array2[index]));
+
+
+      // var array3 = surat.map((obj, index) => {
+      //   console.log(obj)
+      // });
+
+      // var myObject = { 'a': 1, 'b': 2, 'c': 3 };
+
+      // Object.keys(myObject).map(function(key, index, value) {
+      //   console.log(value);
+      // });
+
+      // console.log(myObject);
+
+      // let person = {
+      //     firstName: 'John',
+      //     lastName: 'Doe',
+      //     age: 25,
+      //     ssn: '123-456-2356'
+      // };
+
+
+      // let job = {
+      //     jobTitle: 'JavaScript Developer',
+      //     country: 'USA'
+      // };
+
+
+      // let employee = Object.assign(person, job);
+      // console.log(employee);
+
+
+      // console.log(typeof arti,typeof arti, typeof array3);
+      
     }
+
+    
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="postcss" scoped>
 @font-face {
   font-family: "lpmq";
   src: url(/fonts/lpmq.otf) format("opentype");
@@ -73,4 +191,24 @@ html {
   line-height: 2;
 }
 
+.item {
+  @apply px-8 mx-36 my-8;
+}
+.card {
+  @apply text-2xl py-4 rounded-lg my-8;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.20);
+  .idSurah {
+    @apply text-center;
+    width: 50px;
+  }
+  .nameSurah {
+    @apply px-4 text-right w-full;
+  }
+}
+
+@screen mobile {
+  .item {
+    @apply mx-4;
+  }
+}
 </style>
